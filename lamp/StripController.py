27808e6053a4674ce3cbbe866f0986acf68e58a3
David Greaves <david.greaves@jolla.com>
2021-11-22 14:53:25 +0000

    Support the new StripShow classes and multiple show/strips
    Signed-off-by: David Greaves <david@dgreaves.com>

--- lamp/StripController.py
+++ lamp/StripController.py
@@ -1,7 +1,9 @@
 import asyncio
 
-from .StripShow import StripShow
+from .StripShow import *
 import logging
+import json
+
 logger = logging.getLogger(__name__)
 
 
@@ -23,34 +25,39 @@ class StripController:
     When an MQTT message arrives it stops the current StripShow and
     starts a new one.
 
+    The StripController has a number of substrips each of which has an
+    associated StripShow. A StripShow can control one or more strips
+    so the same show can be present multiple times.
+    Equally multiple instances of the same show can be present on
+    different strips (typically with different paramaters).
+    A show can take over a strip (using the mirror command).
+
     """
 
-    def __init__(self, lamp, strip, config):
+    def __init__(self, mqctrl, strip, config):
         """"""
         self.strip = strip
-        self.lamp = lamp
-        lamp.add_handler(self.msg_handler)
-        lamp.subscribe("named/control/lamp/#")
+        self.mqctrl = mqctrl
+        mqctrl.add_handler(self.msg_handler)
+        mqctrl.subscribe("named/control/lamp/#")
         self.strips = {}
         self.shows = {}
-        for n in config.keys():
-            logger.debug(f"config {n} is {config[n]}")
-            ss = strip.createPixelSubStrip(config[n]["first_pixel"],
-                                           num=config[n]["num_pixels"])
-            self.strips[n] = ss
-            show = StripShow(self, ss, n)
-            self.shows[n] = show
-            show.start()
+        for name in config.keys():
+            logger.debug(f"config {name} is {config[name]}")
+            ss = strip.createPixelSubStrip(config[name]["first_pixel"],
+                                           num=config[name]["num_pixels"])
+            self.strips[name] = ss
         logger.debug(f"strips {self.strips}")
         logger.debug(f"shows {self.shows}")
+        self.effects = []
 
     async def msg_handler(self, topic, payload):
         """
         Message format is:
         named/control/lamp/{NAME}/brightness
         named/control/lamp/{NAME}/state
-        named/control/lamp/{NAME}/painter
         named/control/lamp/{NAME}/strip/{NAME}/painter/{PAINTER}
+        named/control/lamp/{NAME}/strip/{NAME}/mirror/{NAME2}
         """
         logger.debug(f"Handler got msg {topic} {payload}")
         if not topic.startswith("named/control/lamp"):
@@ -66,32 +73,67 @@ class StripController:
             pass
         elif control == "strip":
             sname = topics[2]
-            if not sname in self.shows:
-                logger.alert(f"Strip {sname} not found in lamp {name}")
+            if sname != "all" and sname not in self.strips:
+                logger.warning(f"Strip {sname} not found in lamp {name}")
                 return True
             if topics[3] == "painter":
                 logger.debug(f"Paint strip {sname} {payload}")
-                await self.shows[sname].setPainter(payload)
+                await self.setPainter(sname, payload)
+            if topics[3] == "mirror":
+                logger.debug(f"Paint strip {sname} {payload}")
+                await self.setPainter(payload)
         return True
-        
+
+    async def setPainter(self, sname, rawpayload):
+        """Sets the Painter for one or more strips using a json structure.
+
+        The raw payload is interpreted as a utf-8 json string
+        containing a hash with a 'painter' key and the args to the
+        particular painter.
+        """
+        logger.debug(f"setPainter({rawpayload})")
+        # validate the sname(s) here
+        args = json.loads(rawpayload.decode("utf-8") or "null")
+        try:
+            cls = args["painter"]
+        except (TypeError, KeyError):
+            logger.debug("No 'painter' found in payload")
+            return
+        try:
+            newshow = globals()[cls](self, args)
+        except NameError:
+            logger.debug(f"setPainter: invalid painter class: {cls}")
+
+        if sname == "all":
+            strips = self.strips.keys()
+        else:
+            strips = sname.split(",")
+
+        for sname in strips:
+            s = self.strips[sname]
+            if s in self.shows:
+                await self.shows[s].removeStrip(s)
+            # Now everything has stopped we can set the global painter and args
+            newshow.addStrip(s)
+            self.shows[s] = newshow
+        newshow.start()
+
+        # self.controller.publish(f"strip/{self.name}/painter",
+        #                         self.painter._as_payload())
+    
     def setBrightness(self, b):
         logger.debug(f"Setting brightness to {b}")
         self.strip.setBrightness(b)
         # Now run a frame of the strip show in case it's static
         self.strip.show()
-        self.lamp.publish("named/sensor/lamp/Ballroom/brightness", b)
+        self.mqctrl.publish("named/sensor/lamp/Ballroom/brightness", b)
 
     def publish(self, topic, payload):
         """Publish payload to topic
         Only needs the strip section of the topic
         """
-        self.lamp.publish(f"named/sensor/lamp/Ballroom/{topic}", payload)
-
-    async def run(self):
-        print(self.shows.values())
-        asyncio.gather(self.shows.values())
+        self.mqctrl.publish(f"named/sensor/lamp/Ballroom/{topic}", payload)
 
     def exit(self):
-        for s in self.shows.keys():
-            self.shows[s] = {"painter": "colorWipe",
-                             "colour": (255, 0, 0)}
+        for s in self.strips.values():
+            s.off()
