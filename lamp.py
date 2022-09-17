ec041dc8a189f5505783832c94fd80b53feee7b6
David Greaves <david.greaves@jolla.com>
2022-09-17 17:28:01 +0100

    Provide task information in debug Format
    Signed-off-by: David Greaves <david@dgreaves.com>

--- lamp.py
+++ lamp.py
@@ -13,6 +13,23 @@ from rpi_ws281x import PixelStrip
 import logging
 logger = logging.getLogger(__name__)
 
+class myFormatter(logging.Formatter):
+    def __init__(self, fmt):
+        super().__init__(fmt)
+
+    def format(self, record):
+        res = super().format(record)
+
+        if "(task_id)" in res:
+            try:
+                t = asyncio.current_task()
+                n = t.get_name()
+            except RuntimeError:
+                n = "main"
+            res = res.replace("(task_id)", n)
+
+        return res
+
 async def main():
     #asyncio.get_running_loop().set_exception_handler(handle_exception)
 
@@ -29,7 +46,7 @@ async def main():
 
     ch = logging.StreamHandler()
     ch.setLevel(lvl)
-    ch.setFormatter(logging.Formatter("%(name)s : %(message)s"))
+    ch.setFormatter(myFormatter("%(name)s:(task_id) : %(message)s"))
     for l in modules:
         logging.getLogger(l).addHandler(ch)
         logging.getLogger(l).setLevel(lvl)
